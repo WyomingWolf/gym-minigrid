9af452148227348f1e0d7a3ddc25811087baa373
James <james.w.mock@protonamil.com>
2020-12-02 13:30:16 -0700

    maze atrributes based on probabiltiy

--- gym_minigrid/envs/mazeEnv.py
+++ gym_minigrid/envs/mazeEnv.py
@@ -9,13 +9,14 @@ class MazeEnv(MiniGridEnv):
 
     def __init__(
         self,
-        size=2, # 1 or greater; increase maze size
-        limit=1, # 1 to size; increase room size
-        obstacle_type=None, # toggles lava CHANGE TO PERCENT CHANCE TO ADD LAVA float(0-0.2)
-        ball_level=0, # 0 to 5; increase movable objects CHANGE TO PERCENT CHANCE TO CONVERT TO BALL
-        box2ball=0, # CHANCE TO CONVERT TO BALL float(0-1)
-        door_level=0, # 0 to 3; 0=no doors, 1=unlocked doors, 2=locked doors, 3=locked doors hidden keys CHANGE TO PERCENT CHANCE TO ADD DOOR
-        gap_level=1, # 1 to 3; number of gaps added CHANGE TO PERCENT CHANCE TO KEEP WALL
+        size=3, # int(1-6); increase maze size
+        limit=2, # int(1-size); increase room size
+        lava_prob=0, # float(0-0.2); chance to add lava float(0-0.2)
+        obstacle_level=0, # float(0-5); increase obstacles
+        box2ball=0, # flaot(0-1) chance to convert box to ball
+        door_prob=0, # float(0-0.5); chance to add doors
+        lock_prob=0, # float(0-0.5); chance to add locked doors and keys
+        wall_prob=1, # float(0-1); cahnce to keep wall
         agent_start_pos=(1,1),
         agent_start_dir=0,
         seed=0
@@ -24,16 +25,19 @@ class MazeEnv(MiniGridEnv):
         self.agent_start_dir = agent_start_dir
         self.size = pow(2,size+1)+1
         self.limit = pow(2,limit)+1
-        self.obstacle_type = obstacle_type
-        self.ball_level = ball_level
-        self.door_level = door_level
-        self.gap_level = gap_level
-
+        self.lava_prob = lava_prob
+        self.obstacle_level = obstacle_level
+        self.box2ball = box2ball
+        self.door_prob = door_prob
+        self.lock_prob = lock_prob
+        self.wall_prob = wall_prob
+        
+        '''
         if self.door_level > 1:
             self.lock = True
         else:
             self.lock = False
-
+        '''
         super().__init__(
             grid_size = self.size,
             max_steps = 4*self.size*self.size,
@@ -54,13 +58,20 @@ class MazeEnv(MiniGridEnv):
             self._recursive_division(1, 1, self.size-2, self.limit, 0)
 
         # place moveable objeccts
-        if self.ball_level > 0:
-            num_balls = int(self.size*self.ball_level)
-            for i in range(num_balls):
+        if self.obstacle_level > 0:
+            num_obstacle = int(self.size*self.obstacle_level)
+            for i in range(num_obstacle):
+                # get position
                 x = self._rand_int(0, int((self.size-1)/2))*2+1
                 y = self._rand_int(0, int((self.size-1)/2))*2+1
                 if (x > 1 or y > 1) and self.grid.get(x, y)==None: # can't populate agent start position
-                    self.put_obj(Ball(), x, y)
+                    obj = Box
+                    color = 'blue'
+                    prob = self._rand_float(0,1) # change box to ball
+                    if prob < self.box2ball:
+                        obj = Ball
+                        color = 'yellow'
+                    self.put_obj(obj(color), x, y)
 
         # Place a goal square in the bottom-right corner
         self.put_obj(Goal(), width-2, height-2)
@@ -77,53 +88,41 @@ class MazeEnv(MiniGridEnv):
     def _recursive_division(self, x, y, size, min, lvl):
         div = int((size-1)/2)
 
-        num_gap = (self.gap_level - lvl)
-        if num_gap < 1:
-            num_gap = 1
-        #print(num_gap)
-        # horizontal wall
-        gap1 = 0
-        for j in range(num_gap):
-            # generate gap positions
-            gap1 = self._rand_int(0, div+1)*2
-            while self.grid.get(x+gap1, y+div)!=None:
-                gap1 = self._rand_int(0, div+1)*2
-                #print(gap1)
-
-            if size>7 and self.door_level>0:
-                self.grid.set(x+gap1, y+div, Door(color=COLOR_NAMES[lvl%6], is_locked=self.lock))
-
-        # build walls
+        # hoizontal wall
+        # generate gap positions
+        gap = self._rand_int(0, div+1)*2
         for i in range(size):
-            if self.grid.get(x+i, y+div)==None and i!=gap1:
+            if i%2==1:
                 self.grid.set(x+i, y+div, Wall())
+            else:
+                if self.wall_prob > self._rand_float(0,1) and i!=gap:
+                    self.grid.set(x+i, y+div, Wall())
+                # add doors
+                else:
+                    if self.door_prob > self._rand_float(0,1):
+                        self.grid.set(x+i, y+div, Door(color=self._rand_elem(COLOR_NAMES)))
 
-        gap1 = 0
-        gap2 = 2
         # vertical wall
-        for j in range(num_gap):            
-            # generate gap positions 
-            if div > 2:   
-                gap1 = self._rand_int(0, div)
-                while gap1%2==1 or self.grid.get(x+div, y+gap1)!=None:
-                    gap1 = self._rand_int(0, div)
-                    #print(gap1)
+        # generate gap positions
+        if div > 2: 
+            gap1 = self._rand_int(0, (div+1)/2)*2
+            gap2 = self._rand_int(0, (div+1)/2)*2 + div + 1
+        else:
+            gap1 = 0
+            gap2 = 2
 
-                gap2 = self._rand_int(div+1, size)
-                while gap2%2==1 or self.grid.get(x+div, y+gap2)!=None:
-                    gap2 = self._rand_int(div+1, size)
-                    #print(gap2)
-
-            if size>7 and self.door_level>0:
-                self.grid.set(x+div, y+gap1, Door(color=COLOR_NAMES[lvl%6], is_locked=self.lock))
-                self.grid.set(x+div, y+gap2, Door(color=COLOR_NAMES[lvl%6], is_locked=self.lock))
-
-        # build walls
         for i in range(size):
-            if self.grid.get(x+div, y+i)==None and i!=gap1 and i!=gap2:
+            if i%2==1:
                 self.grid.set(x+div, y+i,  Wall())
-                
-        
+            else:
+                if self.wall_prob > self._rand_float(0,1) and i!=gap1 and i!=gap2:
+                    self.grid.set(x+div, y+i,  Wall())
+                # add doors
+                else:
+                    if self.door_prob > self._rand_float(0,1):
+                        self.grid.set(x+div, y+i, Door(color=self._rand_elem(COLOR_NAMES)))
+
+        '''
         # place keys
         if self.lock==True and size>3 and lvl!=0:
             color = COLOR_NAMES[(lvl-1)%6]
@@ -148,8 +147,10 @@ class MazeEnv(MiniGridEnv):
                         #print((kx==0 and ky==0) or self.grid.get(kx+x, ky+y)!=None)  
                         #print(self.grid.get(kx+x, ky+y))
                     self.put_obj(Box(color='red'), kx+x, ky+y)
+            '''
 
         nlvl = lvl+1
+        # recursion
         if size/2 > min:
             newSize = int((size-1)/2)
             self._recursive_division(x, y, newSize, min, nlvl)
@@ -167,100 +168,71 @@ class MazeEnv(MiniGridEnv):
         div = int((size-1)/2)
 
         # Generate lava wall
-        if self.obstacle_type == Lava:
+        if self.lava_prob > self._rand_float(0,1):
             gap = self._rand_int(0, size)
+
+            # randomize orientation
             dir = self._rand_bool()
-            
             if dir == 0:
                 for i in range(size):
                     if i != gap:
-                        self.grid.set(x+i, y+div, self.obstacle_type())
+                        self.grid.set(x+i, y+div, Lava())
             else:
                 for i in range(size):
                     if i != gap:
-                        self.grid.set(x+div, y+i, self.obstacle_type())  
-        
-        # place keys
-        if self.lock==True and size>6:
-            color = COLOR_NAMES[(lvl)%6]
-            kx = self._rand_int(0, div)*2
-            ky = self._rand_int(0, div)*2
-            while (kx==0 and ky==0) or self.grid.get(kx+x,ky+y)!=None:
-                kx = self._rand_int(0, div)*2
-                ky = self._rand_int(0, div)*2
-                #print(self.grid.get(kx+x,ky+y)!=None)  
-
-            if self.door_level == 2:
-                self.put_obj(Key(color=color), kx+x, ky+y)
-            else:
-                self.put_obj(Box(color='red', contains=Key(color=color)), kx+x, ky+y)
-                
-                for i in range(2):
-                    kx = self._rand_int(0, div)*2
-                    ky = self._rand_int(0, div)*2
-                    
-                    while (kx==0 and ky==0) or self.grid.get(kx+x,ky+y)!=None:
-                        kx = self._rand_int(0, div)*2+1
-                        ky = self._rand_int(0, div)*2
-                        #print(self.grid.get(kx+x,ky+y)!=None)  
-                    self.put_obj(Box(color='red'), kx+x, ky+y)
-
+                        self.grid.set(x+div, y+i, Lava())  
 
 
 # empty room
-class EmptyEnv9x9(MazeEnv):
+class EmptyEnv5x5(MazeEnv):
     def __init__(self, **kwargs):
-        super().__init__(size=3, 
+        super().__init__(size=1, 
                          limit=3, 
-                         obstacle_type=None, 
+                         lava_prob=0.2, 
                          **kwargs)
 
 # simple maze
 class MazeEnv9x9(MazeEnv):
     def __init__(self, **kwargs):
-        super().__init__(size=3, 
-                         limit=1, 
-                         obstacle_type=None, 
+        super().__init__(size=2, 
+                         lava_prob=0.2, 
                          **kwargs)
 
 # medium maze with small rooms
 class MazeEnv17x17EZ(MazeEnv):
     def __init__(self, **kwargs):
-        super().__init__(size=4, 
+        super().__init__(size=3, 
                          limit=2, 
-                         obstacle_type=None, 
+                         lava_prob=0.2, 
                          **kwargs)
 
 # medium maze w/o rooms
 class MazeEnv17x17(MazeEnv):
     def __init__(self, **kwargs):
-        super().__init__(size=4, 
-                         limit=1, 
-                         door_level=3, 
+        super().__init__(size=3,
+                         door_prob=0.3, 
                          **kwargs)
 
 # hard maze w/ small rooms
 class MazeEnv33x33(MazeEnv):
     def __init__(self, **kwargs):
-        super().__init__(size=5, 
-                         limit=2, 
-                         obstacle_type=None, 
+        super().__init__(size=4, 
                          **kwargs)
 
 # large maze w/ large rooms
-class MazeEnv33x33BigRoom(MazeEnv):
+class MazeEnv65x65(MazeEnv):
     def __init__(self, **kwargs):
-        super().__init__(size=4, 
-                         limit=2, 
-                         obstacle_type=Lava, 
-                         ball_level=10,
-                         door_level=3, 
-                         gap_level=1,
+        super().__init__(size=5, 
+                         lava_prob=0.1, 
+                         obstacle_level=2.5,
+                         box2ball=0.2,
+                         door_prob=0.2,
+                         wall_prob=0.9,
                          **kwargs)
 
 register(
-    id='MiniGrid-Maze-9x9-v0',
-    entry_point='gym_minigrid.envs:EmptyEnv9x9'
+    id='MiniGrid-Maze-5x5-v1',
+    entry_point='gym_minigrid.envs:EmptyEnv5x5'
 )
 
 register(
@@ -269,21 +241,21 @@ register(
 )
 
 register(
-    id='MiniGrid-Maze-17x17-v0',
-    entry_point='gym_minigrid.envs:MazeEnv17x17EZ'
-)
-
-register(
     id='MiniGrid-Maze-17x17-v1',
     entry_point='gym_minigrid.envs:MazeEnv17x17'
 )
 
 register(
-    id='MiniGrid-Maze-33x33-v0',
-    entry_point='gym_minigrid.envs:MazeEnv33x33'
+    id='MiniGrid-Maze-17x17-v2',
+    entry_point='gym_minigrid.envs:MazeEnv17x17EZ'
 )
 
 register(
     id='MiniGrid-Maze-33x33-v1',
-    entry_point='gym_minigrid.envs:MazeEnv33x33BigRoom'
+    entry_point='gym_minigrid.envs:MazeEnv33x33'
+)
+
+register(
+    id='MiniGrid-Maze-65x65-v1',
+    entry_point='gym_minigrid.envs:MazeEnv65x65'
 )
